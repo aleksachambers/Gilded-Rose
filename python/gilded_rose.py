b832accc667927d889e9dcd07205ba344302c5ab
Aleksa Chambers <aleksagchambers@gmail.com>
2025-03-20 20:58:31 -0500

    Cleaned up the updat_quality method in gilded_rose.py. Added the conditionals for Conjured items. Created test cases for the Gilded Rose.

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -2,40 +2,77 @@
 
 class GildedRose(object):
 
+    """This code has been updated with the 'raise_quality' and 'lower_quality' methods, 
+    to simplify the process of adding new items to the 'update_quality' method."""
+
     def __init__(self, items):
         self.items = items
 
+    # Raises the quality value of the item.
+    def raise_quality(self, item):
+        if item.quality <= 50:
+            item.quality += 1
+
+
+    # Lowers the quality value of the item. The 'loss_of_value' parameter allows items to degrade at different rates.
+    def lower_quality(self, item, loss_of_value=1):
+        if item.sell_in >= 0:
+            item.quality -= loss_of_value
+        else:
+            item.quality -= 2 * loss_of_value
+            
+        if item.quality < 0:
+                item.quality = 0
+
+
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
+
+            # Meet conditions for Aged Brie.
+            if item.name == "Aged Brie":
+                item.sell_in -= 1
+                self.raise_quality(item)     
+
+
+            # Meet conditions for Sulfuras, Hand of Ragnaros.
+            elif item.name == "Sulfuras, Hand of Ragnaros":
+                item.sell_in = item.sell_in
+                item.quality = 80
+
+
+            # Meet conditions for Backstage Pass. 
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert": 
+                item.sell_in -= 1
+                
+                if item.sell_in < 0:
+                    item.quality = 0
+
+                elif item.sell_in < 6:
+                    self.raise_quality(item)
+                    self.raise_quality(item)
+                    self.raise_quality(item)
+
+                elif item.sell_in < 11:
+                    self.raise_quality(item)
+                    self.raise_quality(item)
+
                 else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                    self.raise_quality(item)
 
 
+            # Condition for Conjured items. 
+            elif "Conjured" in item.name:
+                item.sell_in -= 1
+                self.lower_quality(item, loss_of_value=2)
+
+
+            # Condition for normal items. 
+            else:
+                item.sell_in -= 1
+                self.lower_quality(item, loss_of_value=1)
+
+
+# DON'T TOUCH THIS!!
 class Item:
     def __init__(self, name, sell_in, quality):
         self.name = name
--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -5,12 +5,39 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
+    # This test should fail. The names do not match.
     def test_foo(self):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual("fixme", items[0].name)
+    
+    # This test should fail. An item's quality value cannot be over 50. 
+    def test_max_quality(self):
+        items = [Item("Aged Brie", 53, 55)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(50, items[0].quality)
 
+        # This test should fail. An item's quality cannot go below 0. 
+    def negative_quality(self):
+        items = [Item("Elixir of the Mongoose", 2, -3)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(0, items[0].quality)
+
+    # Checks if conjured items degrade in value correctly. 
+    def test_conjured_items(self):
+        items = [Item("Conjured Mana Cake", 2, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(8, items[0].quality)
+        gilded_rose.update_quality()
+        self.assertEqual(6, items[0].quality)
+        gilded_rose.update_quality()
+        self.assertEqual(2, items[0].quality)
+        gilded_rose.update_quality()
+        self.assertEqual(0, items[0].quality)
         
 if __name__ == '__main__':
     unittest.main()
